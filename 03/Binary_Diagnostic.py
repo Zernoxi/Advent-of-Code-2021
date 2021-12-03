ca77c37407033fce446940c9a730ef7bbfecd847
Zernoxi <tze.chian.kam@gmail.com>
2021-12-03 20:55:35 +1000

    Solution Day 03 now works because of iterators.

--- 03/Binary_Diagnostic.py
+++ 03/Binary_Diagnostic.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+from os import path
+
 def calculation(x, y):
     total = [0, 0]
     for i, bits in enumerate((x, y)):
@@ -7,8 +11,8 @@ def calculation(x, y):
 
 def part_one(data):
     mod_data = zip(*data)
-    gamma = [0 if sum(bits) < len(bits) // 2 else 1 for bits in mod_data]
-    epsilon = (bit ^ 1 for bit in gamma)
+    gamma = [1 if bits.count(1) > bits.count(0) else 0 for bits in mod_data]
+    epsilon = [bit ^ 1 for bit in gamma]
     return calculation(gamma, epsilon)
 
 def part_two(data):
@@ -20,32 +24,27 @@ def part_two(data):
         bits = [bit_col[pos] for bit_col in data]
 
         if bit == 1:
-            common = 1 if sum(bits) > len(bits) // 2 else 0
+            common = 1 if bits.count(1) >= bits.count(0) else 0
         else: # bit == 0
-            common = 1 if sum(bits) <= len(bits) // 2 else 0
-
-        if len(data) == 2:
-            common = common ^ 1
-
+            common = 1 if bits.count(1) < bits.count(0) else 0
         new_data = [data_bits for data_bits in data if data_bits[pos] == common]
         return helper(new_data, pos + 1, bit)
     
     oxygen_gen = helper(mod_data, 0, 1)
-    co2_scrub  = helper(mod_data, 0, 0)
-
+    co2_scrub = helper(mod_data, 0, 0)
     return calculation(oxygen_gen[0], co2_scrub[0])
 
 def main(data):
-    # print(part_one(data))
+    print(part_one(list(data)))
     print(part_two(data))
     
 if __name__ == "__main__":
-    FILE = "./binary.txt"
-    TEST = "./test.txt"
-    with open(FILE) as f:
-        data = (
-            map(int, bits)
+    test_path = path.abspath("./03/test.txt")
+    file_path = path.abspath("./03/binary.txt")
+    with open(file_path) as f:
+        data = [
+            list(map(int, bits))
             for bits in f.read().split()
             if bits != ""
-        )
+        ]
     main(data)
