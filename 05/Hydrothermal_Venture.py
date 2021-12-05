7221e8af195a582dfb9123fee09c3afcfb3d916b
Zernoxi <tze.chian.kam@gmail.com>
2021-12-05 20:43:17 +1000

    Day 5 - Challenge 2 may work.

--- 05/Hydrothermal_Venture.py
+++ 05/Hydrothermal_Venture.py
@@ -19,8 +19,8 @@ def _parse_co_ords(dataset: Data) -> Co_Ordinates:
 def _create_grid(co_ords: Co_Ordinates) -> Grid:
     return tuple(
         [(i, j), 0]
-        for i in range(max(x for x, _ in co_ords) + 1)
-        for j in range(max(y for _, y in co_ords) + 1)
+        for i in range(max(x for x, _ in co_ords)+1)
+        for j in range(max(y for _, y in co_ords)+1)
     )
 
 def return_overlap(grid: Grid) -> int:
@@ -30,18 +30,31 @@ def return_overlap(grid: Grid) -> int:
         if overlap >= 2
     )
 
-def calculation(co_ords: Co_Ordinates, grid: Grid) -> Optional[Any]:
+def function(m, x, c):
+    return m * x + c
+
+# By default only Part one
+def calculation(co_ords: Co_Ordinates, grid: Grid, part: int) -> Grid:
     for (x1, y1), (x2, y2) in (co_ords[i:i+2] for i in range(0, len(co_ords), 2)):
-        if x1 == x2 or y1 == y2:
-            for idx, node in enumerate(grid):
-                if x1 == x2:
+        for idx, node in enumerate(grid):
+            x = node[0][0]
+            y = node[0][1]  
+            if x1 == x2:
+                dy1, dy2 = sorted((y1, y2))
+                if x == x1 and y in range(dy1, dy2+1):
+                    grid[idx][1] = node[1] + 1
+            elif y1 == y2:
+                dx1, dx2 = sorted((x1, x2))
+                if y == y1 and x in range(dx1, dx2+1):
+                    grid[idx][1] = node[1] + 1
+            elif part == 2:
+                m = (y2 - y1) // (x2 - x1)
+                if m == 1 or m == -1:
                     dy1, dy2 = sorted((y1, y2))
-                    if node[0][0] == x1 and node[0][1] in range(dy1, dy2+ 1):
-                        grid[idx][1] = node[1] + 1
-                elif y1 == y2:
-                    dx1, dx2 = sorted((x1, x2))
-                    if node[0][1] == y1 and node[0][0] in range(dx1, dx2 + 1):
-                        grid[idx][1] = node[1] + 1
+                    for dy in range(dy1, dy2+1):
+                        if function(m, x, y1-m*x1) == dy and y == dy:
+                            grid[idx][1] = node[1] + 1
+                            break
     return grid
         
 if __name__ == "__main__":
@@ -54,6 +67,9 @@ if __name__ == "__main__":
 
     co_ords = _parse_co_ords(dataset)
     grid = _create_grid(co_ords)
-    print("Part One: ", return_overlap(calculation(co_ords, grid)))
+    # print("Part One: ", return_overlap(calculation(co_ords, grid, 1)))
+    print("Part Two: ", return_overlap(calculation(co_ords, grid, 2)))
+    # calculation(co_ords, grid, 2)
 
     # Part one only concerned about horizontal and vertical lines: x1 = x2 or y1 = y2
+    # Part two add diagonals
