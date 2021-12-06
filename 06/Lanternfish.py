9caae40ac921033bba242878d197f9dbbe07f3dc
Zernoxi <tze.chian.kam@gmail.com>
2021-12-06 17:14:59 +1000

    Day 6 - Challenge 2 completed. Had to cheat.

--- 06/Lanternfish.py
+++ 06/Lanternfish.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 
-# cSpell: disable
-
 from os import path
 
+def dict_check(dict, key, value=1):
+    if dict.get(key, None) == None:
+        dict[key] = value
+    else:
+        dict[key] += value
+
 def main(data, timeframe):
-    i = 0
-    j = 0
-    k = 0
-    while i < timeframe:
-        while j < len(data):
-            if data[j] > -1:
-                data[j] -= 1
-            j += 1
-        while k < len(data):
-            if data[k] == -1:
-                data.append(NEW_LATERN)
-                data[k] = LATERN
-            k += 1
-        i += 1
-        j = 0
-        k = 0
-    return len(data)
+    for _ in range(timeframe):
+        fishs = {}
+        for lifetime, nums in data.items():
+            if lifetime == 0:
+                dict_check(fishs, 6, nums)
+                dict_check(fishs, 8, nums)
+            else:
+                dict_check(fishs, lifetime-1, nums)
+        data = fishs
+    return sum(data.values())
 
 if __name__ == "__main__":
     dir_path = path.dirname(__file__)
@@ -31,9 +28,13 @@ if __name__ == "__main__":
     # Inclusive of "0"
     LATERN = 6
     NEW_LATERN = 8
+    data_map = {}
 
     # Data input is only on line 1
     with open(file_path) as data:
         dataset = list(map(int, data.readline().rstrip().split(",")))
+        for i in dataset:
+            dict_check(data_map, i)
 
-    print("Part One:", main(dataset, 80))
+    print("Part One:", main(data_map, 80))
+    print("Part Two:", main(data_map, 256))
